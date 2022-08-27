6e7355280633cb9ffcad6a1d42a700581d1a8765
Andrew <andrewludford@hotmail.co.uk>
2022-08-27 21:58:42 +0100

    add code for Delete comments

--- src/components/PostComment.jsx
+++ src/components/PostComment.jsx
@@ -1,3 +1,5 @@
+import { useContext } from "react";
+import { UserContext } from "../contexts/User";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { postCommentAPI } from "../api";
@@ -6,22 +8,28 @@ const PostComment = (props) => {
   const [newComment, setNewComment] = useState("");
   const { article_id } = useParams();
   const [commentPosting, setCommentPosting] = useState(false);
+  const { loggedInUser } = useContext(UserContext);
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
     setCommentPosting(true);
 
-    postCommentAPI(article_id, "jessjelly", newComment).then((res) => {
-      props.setComments((currComments) => [...currComments, res.data.comment]);
-      props.setSingleArticle((currArticle) => {
-        const revisedArticle = { ...currArticle };
-        revisedArticle.comment_count += 1;
-        return revisedArticle;
-      });
-      setNewComment("");
-      setCommentPosting(false);
-    }); //currently uses default user but should improve so that it uses useContext to post username!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    postCommentAPI(article_id, loggedInUser.username, newComment).then(
+      (res) => {
+        props.setComments((currComments) => [
+          ...currComments,
+          res.data.comment,
+        ]);
+        props.setSingleArticle((currArticle) => {
+          const revisedArticle = { ...currArticle };
+          revisedArticle.comment_count += 1;
+          return revisedArticle;
+        });
+        setNewComment("");
+        setCommentPosting(false);
+      }
+    );
   };
 
   if (commentPosting) return <p>Posting comment...</p>;
